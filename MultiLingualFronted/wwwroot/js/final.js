3790ebc81d2492ba6b6553c6abce745e78143101
Sadegh Dehghani <dehghani.prg@outlook.com>
2026-01-30 17:19:43 +0330

    NoEdit

--- MultiLingualFronted/wwwroot/js/final.js
+++ MultiLingualFronted/wwwroot/js/final.js
@@ -3,58 +3,42 @@
     dictionary: {},
 
     load: function (lang) {
-        i18n.currentLang = lang;
-
-        $.getJSON('/i18n/lang-' + lang + '.json')
-            .done(function (data) {
-                i18n.dictionary = data;
-                i18n.applyTranslations();
-                i18n.applyDirection();
-                localStorage.setItem('lang', lang);
-            })
-            .fail(function () {
-                console.error('Language file not found:', lang);
-            });
-    },
-
-    t: function (key) {
-        return i18n.dictionary[key] || key;
+        var self = this;
+        $.getJSON('/lang-' + lang + '.json', function (data) {
+            self.dictionary = data;
+            self.currentLang = lang;
+            console.log('Loaded', lang, 'direction:', data.direction);
+            self.applyTranslations();
+            self.applyDirection();
+            updateLanguageDropdown(); // به‌روزرسانی Dropdown
+        }).fail(function () {
+            console.error('Failed to load language file for ' + lang);
+        });
     },
 
     applyTranslations: function () {
-
         $('[data-i18n]').each(function () {
             var key = $(this).data('i18n');
-            $(this).text(i18n.t(key));
+            var translation = i18n.dictionary[key] || key;
+            $(this).text(translation);
         });
-
-        $('[data-i18n-placeholder]').each(function () {
-            var key = $(this).data('i18n-placeholder');
-            $(this).attr('placeholder', i18n.t(key));
-        });
-
-      
-        $('html').attr('lang', i18n.currentLang);
     },
-
 
     applyDirection: function () {
         var dir = i18n.dictionary.direction || 'rtl';
+        console.log('Setting dir to:', dir); // لاگ برای بررسی
         $('html')
             .attr('dir', dir)
             .attr('lang', i18n.currentLang);
         $('body').removeClass('rtl ltr').addClass(dir);
 
-        updateLanguageDropdown();
-
+        // سوئیچ فایل CSS بوت‌استرپ
         var cssPath = dir === 'rtl'
             ? '/lib/bootstrap/dist/css/bootstrap.rtl.min.css'
             : '/lib/bootstrap/dist/css/bootstrap.min.css';
         $('#bootstrap-css').attr('href', cssPath);
     }
-
 };
-
 
 function updateLanguageDropdown() {
     var lang = i18n.currentLang;
@@ -63,3 +47,9 @@ function updateLanguageDropdown() {
     $('#currentLanguageFlag').text(flag);
     $('#currentLanguageName').text(name);
 }
+
+// بارگذاری زبان پیش‌فرض
+$(document).ready(function () {
+    i18n.load('fa');
+    updateLanguageDropdown();
+});
